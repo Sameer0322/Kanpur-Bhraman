4093d635d1c9cb66e37493d179c8a0095c9573e0
Sameer0322 <116619265+Sameer0322@users.noreply.github.com>
2025-04-24 14:40:13 +0530

    Update server

--- backend/server.js
+++ backend/server.js
@@ -48,5 +48,5 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🌐 Server is running on http://localhost:${PORT}`);
+  console.log(`🌐 Server is running. `);
 });
